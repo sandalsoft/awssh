d2c23a276f95dc6be02a3a09cf89c512e43572aa
Eric Nelson <eric@sandalsoft.com>
2016-10-06 13:12:29 -0500

    + Added Ebs class - Class definition and exports working - Enums working

--- awssh.js
+++ awssh.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
-var AWS = require("aws-butler");
- 
-var ec2 = new AWS.EC2({ region: "us-east-1" });
- 
-// example using promise 
+var Placement = require('./lib/Placement.js')
+
+var AWS = require('aws-butler')
+var ec2 = new AWS.EC2({ region: 'us-east-1' })
+
+var p = new Placement(Placement.AvailabilityZone.us_east_1a, 'group', 'ten')
+console.log('p: ' + p.toString())
+
+// example using promise
 ec2.describeInstances()
-  .then( function(data) {
-    console.log(JSON.stringify(data.Reservations, null, 2)); // data contains standard AWS response 
+  .then(function (data) {
+    console.log(JSON.stringify(data, null, 2)) // data contains standard AWS response
   })
-  .catch( function(err) {
-    throw err;
+  .catch(function (err) {
+    throw err
   })
-  .done();
- 
+  .done()
