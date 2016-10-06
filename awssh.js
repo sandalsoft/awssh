593fec78fbea76d1a2fc7bdae32e95371454750c
Eric Nelson <eric@sandalsoft.com>
2016-10-06 13:54:43 -0500

    + added chalk

--- awssh.js
+++ awssh.js
@@ -2,18 +2,18 @@
 
 var Placement = require('./lib/Placement.js')
 
-var AWS = require('aws-butler')
-var ec2 = new AWS.EC2({ region: 'us-east-1' })
-
 var p = new Placement(Placement.AvailabilityZone.us_east_1a, 'group', 'ten')
 console.log('p: ' + p.toString())
 
+// var AWS = require('aws-butler')
+// var ec2 = new AWS.EC2({ region: 'us-east-1' })
+
 // example using promise
-ec2.describeInstances()
-  .then(function (data) {
-    console.log(JSON.stringify(data, null, 2)) // data contains standard AWS response
-  })
-  .catch(function (err) {
-    throw err
-  })
-  .done()
+// ec2.describeInstances()
+//   .then(function (data) {
+//     console.log(JSON.stringify(data, null, 2)) // data contains standard AWS response
+//   })
+//   .catch(function (err) {
+//     throw err
+//   })
+//   .done()
